6f67146164b86f10898548a036f39d6d970af041
Sanjeev-tech1 <133850705+Sanjeev-tech1@users.noreply.github.com>
2025-04-07 00:30:12 +0530

    Add files via upload

---
+++ full_daimond.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+  
+int main () {
+    int  n=9;
+    int x=n/2+1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(j>=x && j<=n-x+1)
+            cout<<"*";
+            else
+            cout<<" ";
+        }
+        if(i>=n/2)
+        x++;
+        else
+        x--;
+        cout<<endl;
+
+    }
+  
+  
+return 0;
+}
